2ac1aaecd71eb0ab33d7b2ab3e100cafd09685cd
patchwork480 <todd.scott.rule@gmail.com>
2016-06-18 11:37:18 -0700

    Working on wday

--- src/pf_wday.c
+++ src/pf_wday.c
@@ -30,6 +30,61 @@ const GPathInfo PF_WDAY_BORDER_INFO = {
 
 
 
+#define GLYPH_ROWS		7
+#define GLYPH_ROW_WID	5
+
+const char *_XXX_ = ".***.";
+const char *X___X = "*...*";
+const char *XXXXX = "*****";
+const char *XXXX_ = "****.";
+const char *X____ = "*....";
+const char *XXX__ = "***..";
+const char *X__X_ = "*..*.";
+const char *X__XX = "*..**";
+const char *_XXXX = ".****";
+const char *__X__ = "..*..";
+const char *___X_ = "...*.";
+const char *XX_XX = "**.**";
+const char *X_X_X = "*.*.*";
+const char *XX__X = "**..*";
+const char *_XX_X = ".**.*";
+const char *____X = "....*";
+const char *_X_X_ = ".*.*.";
+const char *_X___ = ".*...";
+
+
+char *strupr (char *str) {
+		char *s;
+		for(s = str; *s; s++) {
+				if(('a' <= *s) && (*s <= 'z')) {
+						*s = 'A' + (*s - 'a');
+				}
+		}
+		return str;
+}
+
+
+void wday_letter ( GContext *ctx, char letter, int x, int y ) {
+		if( empty(PF_WKDAY_LETTERS[letter]) ) {
+				return;
+		}
+		char *array[] = PF_WKDAY_LETTERS[letter];
+		int px, py;
+		for( int r = 0; r < GLYPH_ROWS; r++ ) {
+				for( int c = 0; c < GLYPH_ROW_WID; c++ ) {
+						if( array[r][c] == '*' ) {
+								py = y + ( r * 2 );
+								px = x + ( c * 2 );
+								pts = array( px,py,  px+1,py,  px+1,py+1,  px,py+1,  px,py );
+								gpath_draw_filled(ctx, pts, 5, black);
+								gpath_draw_outline(ctx, pts, 5, $white);
+						}
+				}
+		}
+}
+
+
+
 void draw_wday (Layer *layer, GContext *ctx) {
 		GRect bounds = layer_get_bounds(layer);
 		snprintf(message_buffer, sizeof(message_buffer), "draw_wday: %d,%d:%dx%d (%s)",
@@ -38,11 +93,8 @@ void draw_wday (Layer *layer, GContext *ctx) {
 		APP_LOG(APP_LOG_LEVEL_DEBUG, message_buffer);
 
 		snprintf(message_buffer, sizeof(message_buffer), " - Drawing: ``%s''",
-				wday_buffer);
+				strupr(wday_buffer));
 		APP_LOG(APP_LOG_LEVEL_DEBUG, message_buffer);
-
-		// graphics_context_set_fill_color(ctx, PF_BACKGND);
-		// graphics_fill_rect(ctx, bounds, 0, GCornerNone);
 
 		if(PF_WDAY_BORDER==NULL) {
 				PF_WDAY_BORDER = gpath_create(&PF_WDAY_BORDER_INFO);
@@ -50,13 +102,13 @@ void draw_wday (Layer *layer, GContext *ctx) {
 		graphics_context_set_stroke_color(ctx, PF_BORDER);
 		gpath_draw_outline(ctx, PF_WDAY_BORDER);
 
-		graphics_context_set_text_color(ctx, PF_FOREGND);
-		graphics_draw_text(ctx, wday_buffer,
-						fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD),
-						bounds,
-						GTextOverflowModeWordWrap,
-						GTextAlignmentCenter,
-						NULL);
+		graphics_context_set_fill_color(ctx, PF_FOREGND);
+		graphics_context_set_stroke_color(ctx, PF_FOREGND);
+
+		for( int k = 0; k < strlen(wday_buffer); k++ ) {
+				wday_letter( ctx, wday_buffer[k],
+						PF_WKDAY_LETTER_X + (k * PF_WKDAY_LETTER_W), PF_WKDAY_LETTER_Y );
+		}
 }
 
 /* EOF */
--- src/pf_wday.h
+++ src/pf_wday.h
@@ -8,6 +8,11 @@
 #define PF_WDAY_W		48
 #define PF_WDAY_H		20
 
+#define PF_WDAY_LETTER_W 13
+#define PF_WDAY_LETTER_H 14
+#define PF_WDAY_LETTER_Y PF_WDAY_Y + 3
+#define PF_WDAY_LETTER_X PF_WDAY_X + 6
+
 
 extern Layer *wday_layer;
 extern char message_buffer[64];
