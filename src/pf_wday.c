e6fd8cddb657458fbacf064ff352cdb216f18597
patchwork480 <todd.scott.rule@gmail.com>
2016-06-18 13:22:26 -0700

    Finished wday

--- src/pf_wday.c
+++ src/pf_wday.c
@@ -7,6 +7,17 @@ char wday_buffer[12];
 int last_wday;
 
 
+char *strupr (char *str) {
+		char *s;
+		for(s = str; *s; s++) {
+				if(('a' <= *s) && (*s <= 'z')) {
+						*s = 'A' + (*s - 'a');
+				}
+		}
+		return str;
+}
+
+
 void init_wday () {
 		last_wday = -1;
 }
@@ -16,7 +27,6 @@ void update_wday (struct tm *tick_wday, TimeUnits units_changed) {
 		APP_LOG(APP_LOG_LEVEL_DEBUG, "update_wday");
 		layer_mark_dirty(wday_layer);
 }
-
 
 
 GPath *PF_WDAY_BORDER = NULL;
@@ -29,86 +39,68 @@ const GPathInfo PF_WDAY_BORDER_INFO = {
 	} };
 
 
-
-#define GLYPH_ROWS		7
-#define GLYPH_ROW_WID	5
-
-const char *_XXX_ = ".***.";
-const char *X___X = "*...*";
-const char *XXXXX = "*****";
-const char *XXXX_ = "****.";
-const char *X____ = "*....";
-const char *XXX__ = "***..";
-const char *X__X_ = "*..*.";
-const char *X__XX = "*..**";
-const char *_XXXX = ".****";
-const char *__X__ = "..*..";
-const char *___X_ = "...*.";
-const char *XX_XX = "**.**";
-const char *X_X_X = "*.*.*";
-const char *XX__X = "**..*";
-const char *_XX_X = ".**.*";
-const char *____X = "....*";
-const char *_X_X_ = ".*.*.";
-const char *_X___ = ".*...";
-
-
-char *strupr (char *str) {
-		char *s;
-		for(s = str; *s; s++) {
-				if(('a' <= *s) && (*s <= 'z')) {
-						*s = 'A' + (*s - 'a');
-				}
-		}
-		return str;
-}
+const char *PF_WDAY_LETTERS[26][PF_GLYPH_ROWS] = {
+		{ ".***.", "*...*", "*...*", "*****", "*...*", "*...*", "*...*" },
+		{ "****.", "*...*", "*...*", "****.", "*...*", "*...*", "****." },
+		{ ".***.", "*...*", "*....", "*....", "*....", "*...*", ".***." },
+		{ "***..", "*..*.", "*...*", "*...*", "*...*", "*..*.", "***.." },
+		{ "*****", "*....", "*....", "****.", "*....", "*....", "*****" },
+		{ "*****", "*....", "*....", "****.", "*....", "*....", "*...." },
+		{ ".***.", "*...*", "*....", "*..**", "*....", "*...*", ".****" },
+		{ "*...*", "*...*", "*...*", "*****", "*...*", "*...*", "*...*" },
+		{ "*****", "..*..", "..*..", "..*..", "..*..", "..*..", "*****" },
+		{ "*****", "...*.", "...*.", "...*.", "*..*.", "*..*.", ".***." },
+		{ "*...*", "*...*", "*..*.", "***..", "*..*.", "*...*", "*...*" },
+		{ "*....", "*....", "*....", "*....", "*....", "*....", "*****" },
+		{ "*...*", "**.**", "*.*.*", "*.*.*", "*...*", "*...*", "*...*" },
+		{ "*...*", "**..*", "*.*.*", "*.*.*", "*.*.*", "*..**", "*...*" },
+		{ ".***.", "*...*", "*...*", "*...*", "*...*", "*...*", ".***." },
+		{ "****.", "*...*", "*...*", "****.", "*....", "*....", "*...." },
+		{ ".***.", "*...*", "*...*", "*...*", "*.*.*", "*..*.", ".**.*" },
+		{ "****.", "*...*", "*...*", "****.", "*..*.", "*...*", "*...*" },
+		{ ".***.", "*...*", "*....", ".***.", "....*", "*...*", ".***." },
+		{ "*****", "..*..", "..*..", "..*..", "..*..", "..*..", "..*.." },
+		{ "*...*", "*...*", "*...*", "*...*", "*...*", "*...*", ".***." },
+		{ "*...*", "*...*", "*...*", ".*.*.", ".*.*.", ".*.*.", "..*.." },
+		{ "*...*", "*...*", "*.*.*", "*.*.*", "*.*.*", "*.*.*", ".*.*." },
+		{ "*...*", "*...*", ".*.*.", "..*..", ".*.*.", "*...*", "*...*" },
+		{ "*...*", "*...*", ".*.*.", "..*..", "..*..", "..*..", "..*.." },
+		{ "*****", "....*", "...*.", "..*..", ".*...", "*....", "*****" },
+};
 
 
-void wday_letter ( GContext *ctx, char letter, int x, int y ) {
-		if( empty(PF_WKDAY_LETTERS[letter]) ) {
-				return;
-		}
-		char *array[] = PF_WKDAY_LETTERS[letter];
+void wday_letter (GContext *ctx, const char letter, const int x, const int y ) {
+		int index = ((int)letter) - ((int)'A');
 		int px, py;
-		for( int r = 0; r < GLYPH_ROWS; r++ ) {
-				for( int c = 0; c < GLYPH_ROW_WID; c++ ) {
-						if( array[r][c] == '*' ) {
+		for( int r = 0; r < PF_GLYPH_ROWS; r++ ) {
+				for( int c = 0; c < PF_GLYPH_ROW_WID; c++ ) {
+						if( PF_WDAY_LETTERS[index][r][c] == '*' ) {
 								py = y + ( r * 2 );
 								px = x + ( c * 2 );
-								pts = array( px,py,  px+1,py,  px+1,py+1,  px,py+1,  px,py );
-								gpath_draw_filled(ctx, pts, 5, black);
-								gpath_draw_outline(ctx, pts, 5, $white);
+								graphics_draw_pixel(ctx, (GPoint){px,py} );
+								graphics_draw_pixel(ctx, (GPoint){px+1,py} );
+								graphics_draw_pixel(ctx, (GPoint){px,py+1} );
+								graphics_draw_pixel(ctx, (GPoint){px+1,py+1} );
 						}
 				}
 		}
 }
 
 
-
 void draw_wday (Layer *layer, GContext *ctx) {
-		GRect bounds = layer_get_bounds(layer);
-		snprintf(message_buffer, sizeof(message_buffer), "draw_wday: %d,%d:%dx%d (%s)",
-				bounds.origin.x, bounds.origin.y, bounds.size.w, bounds.size.h,
-				layer_get_hidden(layer) ? "true" : "false");
-		APP_LOG(APP_LOG_LEVEL_DEBUG, message_buffer);
-
-		snprintf(message_buffer, sizeof(message_buffer), " - Drawing: ``%s''",
-				strupr(wday_buffer));
-		APP_LOG(APP_LOG_LEVEL_DEBUG, message_buffer);
-
 		if(PF_WDAY_BORDER==NULL) {
 				PF_WDAY_BORDER = gpath_create(&PF_WDAY_BORDER_INFO);
 		}
 		graphics_context_set_stroke_color(ctx, PF_BORDER);
 		gpath_draw_outline(ctx, PF_WDAY_BORDER);
 
-		graphics_context_set_fill_color(ctx, PF_FOREGND);
 		graphics_context_set_stroke_color(ctx, PF_FOREGND);
 
-		for( int k = 0; k < strlen(wday_buffer); k++ ) {
+		for( int k = 0; k < PF_WDAY_WID; k++ ) {
 				wday_letter( ctx, wday_buffer[k],
-						PF_WKDAY_LETTER_X + (k * PF_WKDAY_LETTER_W), PF_WKDAY_LETTER_Y );
+						PF_WDAY_LETTER_X + (k * PF_WDAY_LETTER_W), PF_WDAY_LETTER_Y );
 		}
 }
+
 
 /* EOF */
--- src/pf_wday.h
+++ src/pf_wday.h
@@ -10,12 +10,16 @@
 
 #define PF_WDAY_LETTER_W 13
 #define PF_WDAY_LETTER_H 14
-#define PF_WDAY_LETTER_Y PF_WDAY_Y + 3
-#define PF_WDAY_LETTER_X PF_WDAY_X + 6
+#define PF_WDAY_LETTER_Y 3
+#define PF_WDAY_LETTER_X 6
+
+
+#define PF_WDAY_WID        	3
+#define PF_GLYPH_ROWS		7
+#define PF_GLYPH_ROW_WID	5
 
 
 extern Layer *wday_layer;
-extern char message_buffer[64];
 
 
 extern char wday_buffer[12];
